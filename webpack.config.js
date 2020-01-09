2e707f1f3cb6104366ec5322d98a8e729d79a3ed
Christopher Rogers <chrissrogers@gmail.com>
2020-01-08 20:49:53 -0500

    Fixes quote format

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
