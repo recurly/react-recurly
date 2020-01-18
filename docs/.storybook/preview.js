137ca2b1b5af099a7f6a4e909662327ba24fa3f2
Christopher Rogers <chrissrogers@gmail.com>
2020-01-18 03:55:27 -0800

    Adds link to GitHub repo

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -3,11 +3,16 @@ import { configure, addParameters } from '@storybook/react';
 import { create } from '@storybook/theming';
 import { version } from '../../package.json';
 
+import 'highlight.js/styles/tomorrow-night.css';
+
 const theme = create({
   base: 'light',
   brandTitle: `react-recurly ${version}`,
   // brandImage: 'https://recurly.com/img2/brand/wordmark-1@2x.png'
+
   fontBase: '"Open Sans", Helvetica, sans-serif',
+  appBg: 'rgb(250, 250, 250)',
+  colorSecondary: '#5191b7'
 });
 
 addParameters({
