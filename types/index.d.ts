3d06fcc45b517d6bef79b4f41c58b3727f2394f4
Andrei Socaciu <andrewsocaciu@gmail.com>
2020-11-17 18:22:47 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -97,7 +97,7 @@ export type UseCheckoutPricingState = {
   price: CheckoutPrice;
   loading: boolean;
 };
-                                   
+
 export type SetCheckoutPricing = (input: UseCheckoutPricingInput | ((prevState: UseCheckoutPricingInput) => UseCheckoutPricingInput)) => void;
 
 export type UseCheckoutPricingReturn = [UseCheckoutPricingState, SetCheckoutPricing];
