80af8e92526fb59418f84cd62fe2c5b782cf6a00
Alexander <aleyva@recurly.com>
2023-03-17 15:40:53 -0300

    add dev dependency babel/plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '@babel/preset-env'
   ],
   'plugins': [
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime',
   ]
 };
