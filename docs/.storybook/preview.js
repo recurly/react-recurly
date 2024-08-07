2f905d3712dce1355ac57b95cf2609b846f2babc
Chris Smola <952681+Smolations@users.noreply.github.com>
2024-08-07 11:33:44 -0400

    [add-a11y-addon] installs storybook addon

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -29,7 +29,15 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/
       }
-    }
+    },
+    a11y: {
+      // axe-core configurationOptions (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1)
+      // config: {},
+      // axe-core optionsParameter (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter)
+      // options: {},
+      // optional flag to prevent the automatic check
+      // manual: true,
+    },
   }
 };
 
