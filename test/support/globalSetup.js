47844181bbe5d2b354661e8d0a88a2b2908b09a6
Wendy Arango <warangochavarria@recurly.com>
2025-11-26 11:29:18 -0500

    fix: Change to fixture server
    Signed-off-by: Wendy Arango <warangochavarria@recurly.com>

--- test/support/globalSetup.js
+++ test/support/globalSetup.js
@@ -2,5 +2,5 @@
 // jest.setup.js, otherwise, import 'recurly.js/test/server' fails because
 // it uses async/await.
 require('regenerator-runtime/runtime');
-require('@recurly/public-api-test-server');
+require('@recurly/public-api-test-server/packages/public-api-fixture-server');
 export default async function () {}
